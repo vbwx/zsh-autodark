df902180b6794a34af930c48fe7b8f2f58702f36
Bernhard Waldbrunner <vbwx01@gmail.com>
2026-04-29 22:05:30 +0200

    Smartly switch between light and dark themes

--- zsh-autodark.plugin.zsh
+++ zsh-autodark.plugin.zsh
@@ -1,7 +1,21 @@
 # if [[ "$(uname -s)" == "Darwin" ]]; then
 	if [[ $(defaults read -g AppleInterfaceStyle 2> /dev/null) == Dark ]]; then
 		osascript -e 'tell application "Terminal"
-			set current settings of tabs of windows to settings set "'$DARK_THEME'"
+			repeat with t in tabs of windows
+				set n to name of current settings of t
+				if n is equal to "'$LIGHT_THEME'" then
+					set current settings of t to settings set "'$DARK_THEME'"
+				end if
+			end repeat
+		end tell'
+	else
+		osascript -e 'tell application "Terminal"
+			repeat with t in tabs of windows
+				set n to name of current settings of t
+				if n is equal to "'$DARK_THEME'" then
+					set current settings of t to settings set "'$LIGHT_THEME'"
+				end if
+			end repeat
 		end tell'
 	fi
 # fi
