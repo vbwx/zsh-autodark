86ea4270b06c282916dab97effda9378f10ae0f2
Bernhard Waldbrunner <vbwx01@gmail.com>
2026-05-02 21:34:53 +0200

    Rewrite plugin script
    - Use local variables to get rid of redundant code
    - Prevent accidental launch of Terminal
    - Enforce a specific theme by setting shell variable FORCE_DARK_THEME

--- zsh-autodark.plugin.zsh
+++ zsh-autodark.plugin.zsh
@@ -1,21 +1,20 @@
-# if [[ "$(uname -s)" == "Darwin" ]]; then
-	if [[ $(defaults read -g AppleInterfaceStyle 2> /dev/null) == Dark ]]; then
+function () {
+	# if [[ "$(uname -s)" == "Darwin" ]] {
+		if [[ $FORCE_DARK_THEME == true ||
+			(-z $FORCE_DARK_THEME && $(defaults read -g AppleInterfaceStyle 2> /dev/null) == "Dark") ]] {
+			local oldtheme=$LIGHT_THEME newtheme=$DARK_THEME
+		} else {
+			local oldtheme=$DARK_THEME newtheme=$LIGHT_THEME
+		}
 		osascript -e 'tell application "Terminal"
-			repeat with t in tabs of windows
-				set n to name of current settings of t
-				if n is equal to "'$LIGHT_THEME'" then
-					set current settings of t to settings set "'$DARK_THEME'"
-				end if
-			end repeat
+			if it is running then
+				repeat with t in tabs of windows
+					set n to name of current settings of t
+					if n is equal to "'$oldtheme'" then
+						set current settings of t to settings set "'$newtheme'"
+					end if
+				end repeat
+			end if
 		end tell'
-	else
-		osascript -e 'tell application "Terminal"
-			repeat with t in tabs of windows
-				set n to name of current settings of t
-				if n is equal to "'$DARK_THEME'" then
-					set current settings of t to settings set "'$LIGHT_THEME'"
-				end if
-			end repeat
-		end tell'
-	fi
-# fi
+	# }
+}
